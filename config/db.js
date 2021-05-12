140eda5e015ed203b27931951e1506f3dda8418e
addegbenga <addegbenga@gmail.com>
2021-05-12 15:26:59 +0100

    changes

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
   try {
 
     //input your db url in .env file 
-    const conn = await mongoose.connect(process.env.DB_URI, { //  MONGO_URI
+    const conn = await mongoose.connect("mongodb+srv://addegbenga:samson@14@adeyemi.lqnhw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { //  MONGO_URI
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
